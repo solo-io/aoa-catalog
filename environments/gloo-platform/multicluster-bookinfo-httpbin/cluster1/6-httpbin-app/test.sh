2ff08cb656a7bba3e965ca8505ec739ea2302b84
Alex <alexbrucely@gmail.com>
2023-02-21 15:24:43 -0800

    integrate homer with gloo platform environments

--- environments/gloo-platform/multicluster-bookinfo-httpbin/cluster1/6-httpbin-app/test.sh
+++ environments/gloo-platform/multicluster-bookinfo-httpbin/cluster1/6-httpbin-app/test.sh
@@ -2,13 +2,3 @@
 
 # wait for completion of httpbin install
 $SCRIPT_DIR/tools/wait-for-rollout.sh deployment in-mesh httpbin 10 ${cluster_context}
-
-# echo port-forward commands
-echo 
-echo "access argocd dashboard:"
-echo "kubectl port-forward svc/argocd-server -n argocd 9999:443 --context ${cluster_context}"
-echo
-echo "navigate to http://localhost:9999/argo in your browser for argocd"
-echo
-echo "username: admin"
-echo "password: solo.io"
