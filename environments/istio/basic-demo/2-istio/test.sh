fbc57149376de6f73f583d941981f1f2b4807e30
Alex <alexbrucely@gmail.com>
2023-02-28 10:18:33 -0800

    add istio/basic-demo environment

---
+++ environments/istio/basic-demo/2-istio/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment istio-ingressgateway istio-system 10 ${cluster_context}
+#$SCRIPT_DIR/tools/wait-for-rollout.sh deployment istio-eastwestgateway-1-15 istio-eastwest 10 ${cluster_context}
