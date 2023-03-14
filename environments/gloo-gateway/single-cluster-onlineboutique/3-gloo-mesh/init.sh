e5b6366f6d01eb035abcd0cf5639b51069330d1d
Alex <alexbrucely@gmail.com>
2023-03-14 11:02:42 -0700

    remove unnecessary

--- environments/gloo-gateway/single-cluster-onlineboutique/3-gloo-mesh/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "wave description:"
-echo "deploy and register gloo-mesh agent and addons"
-
-if [ "${environment_overlay}" == "ocp" ] ; then 
-     oc --context ${cluster_context} adm policy add-scc-to-group anyuid system:serviceaccounts:gloo-mesh-addons
-     oc --context ${cluster_context} adm policy add-scc-to-group nonroot system:serviceaccounts:gloo-mesh-prometheus
-  fi
--- environments/gloo-gateway/single-cluster-onlineboutique/4-istio/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ISTIO_REVISION="1-16"
-
-$SCRIPT_DIR/tools/wait-for-rollout.sh deployment istio-ingressgateway-${ISTIO_REVISION} istio-gateways 10 ${cluster_context}
