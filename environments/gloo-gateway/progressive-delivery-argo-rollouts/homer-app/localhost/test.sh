13df571eedfbca87b6e639e75b3b1567f4685924
Alex <alexbrucely@gmail.com>
2023-05-26 17:33:20 -0700

    update tests

--- environments/gloo-gateway/progressive-delivery-argo-rollouts/homer-app/localhost/test.sh
+++ environments/gloo-gateway/progressive-delivery-argo-rollouts/homer-app/localhost/test.sh
@@ -9,9 +9,5 @@ echo "if running locally with K3d:"
 echo "access the dashboard at https://localhost/homer"
 echo
 echo "If using LoadBalancer External-IP:"
-echo "access the dashboard at https://$(kubectl get svc -n istio-gateways --selector=istio=ingressgateway -o jsonpath='{.items[*].status.loadBalancer.ingress[0].ip}')"
-echo
-echo "Additional details on hostname entries for this demo environment will be provided in the Welcome section, but is also described in the README section below:"
-echo "https://github.com/solo-io/aoa-catalog/tree/main/environments/gloo-gateway/single-cluster-bookinfo#application-description"
-echo
+echo "access the dashboard at https://$(kubectl get svc -n istio-gateways --selector=istio=ingressgateway -o jsonpath='{.items[*].status.loadBalancer.ingress[0].ip}')/homer"
 echo
