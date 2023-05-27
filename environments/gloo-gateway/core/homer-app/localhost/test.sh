17dc7a2eeba99fe05824178ea8c3c3733827072b
Alex <alexbrucely@gmail.com>
2023-05-26 17:05:01 -0700

    test core

--- environments/gloo-gateway/core/homer-app/localhost/test.sh
+++ environments/gloo-gateway/core/homer-app/localhost/test.sh
@@ -6,7 +6,7 @@ echo
 echo "Installation complete:"
 echo "A homepage has been exposed at the Gloo Gateway wildcard '*' host to simplify navigation"
 echo "if running locally with K3d:" 
-echo "access the dashboard at https://localhost"
+echo "access the dashboard at https://localhost/homer"
 echo
 echo "If using LoadBalancer External-IP:"
 echo "access the dashboard at https://$(kubectl get svc -n istio-gateways --selector=istio=ingressgateway -o jsonpath='{.items[*].status.loadBalancer.ingress[0].ip}')"
