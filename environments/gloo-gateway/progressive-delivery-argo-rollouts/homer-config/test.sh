0837337b60b8f252d3fd69f37acd4d58949017e8
Alex <alexbrucely@gmail.com>
2023-07-05 09:56:51 -0700

    update argo rollouts demo to use solo plugin

--- environments/gloo-gateway/progressive-delivery-argo-rollouts/homer-config/test.sh
+++ environments/gloo-gateway/progressive-delivery-argo-rollouts/homer-config/test.sh
@@ -10,6 +10,6 @@ echo "If using LoadBalancer External-IP:"
 echo "access the dashboard at https://$(kubectl get svc -n istio-gateways --selector=istio=ingressgateway -o jsonpath='{.items[*].status.loadBalancer.ingress[0].ip}')"
 echo
 echo "Additional details on hostname entries for this demo environment will be provided in the Welcome section, but is also described in the README section below:"
-echo "https://github.com/solo-io/aoa-catalog/tree/main/environments"
+echo "https://github.com/solo-io/aoa-catalog/tree/main/environments/gloo-gateway/single-cluster-bookinfo#application-description"
 echo
 echo
