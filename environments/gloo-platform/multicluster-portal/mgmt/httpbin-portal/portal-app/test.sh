793624ce7f98e7f477ef36203b3860e424732d0d
JM Huibonhoa <jm.huibonhoa@solo.io>
2023-07-20 21:33:19 -0400

    moving things around part 2

--- environments/gloo-platform/multicluster-portal/mgmt/httpbin-portal/portal-app/test.sh
+++ environments/gloo-platform/multicluster-portal/mgmt/httpbin-portal/portal-app/test.sh
@@ -2,4 +2,4 @@
 
 #$SCRIPT_DIR/tools/wait-for-rollout.sh deployment pets-rest-api petstore 5 ${cluster_context}
 #$SCRIPT_DIR/tools/wait-for-rollout.sh deployment tracks-rest-api tracks 5 ${cluster_context}
-#$SCRIPT_DIR/tools/wait-for-rollout.sh deployment httpbin httpbin 5 ${cluster_context}
+#$SCRIPT_DIR/tools/wait-for-rollout.sh deployment portal-frontend httpbin 5 ${cluster_context}
