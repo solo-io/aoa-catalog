1391fca8c19309b201e30b20af6ab0d8c6a5088e
Ashley Wang <ashley.wang@solo.io>
2023-09-06 10:05:51 -0400

    update

--- environments/gloo-platform/core/cluster1/gloo-platform/init.sh
+++ environments/gloo-platform/core/cluster1/gloo-platform/init.sh
@@ -7,7 +7,7 @@ echo "deploy and register gloo-mesh agent and addons"
 if [[ ${gloo_mesh_version} == "" ]]
   then
     # provide gloo_mesh_version variable
-    echo "Please provide the gloo_mesh_version to use (i.e. 2.5.0-beta0-2023-09-04-main-f41f5ebf4):"
+    echo "Please provide the gloo_mesh_version to use (i.e. 2.5.0-beta0-2023-09-05-main-2a0fac6d6):"
     read gloo_mesh_version
 fi
 
@@ -70,7 +70,7 @@ spec:
                     namespace: gloo-mesh
 
     repoURL: https://storage.googleapis.com/gloo-platform-dev/platform-charts/helm-charts
-    targetRevision: 2.5.0-beta0-2023-09-04-main-f41f5ebf4
+    targetRevision: 2.5.0-beta0-2023-09-05-main-2a0fac6d6
   syncPolicy:
     automated:
       prune: true
@@ -176,7 +176,7 @@ spec:
                       endpoint: 0.0.0.0:4318
                   
     repoURL: https://storage.googleapis.com/gloo-platform-dev/platform-charts/helm-charts
-    targetRevision: 2.5.0-beta0-2023-09-04-main-f41f5ebf4
+    targetRevision: 2.5.0-beta0-2023-09-05-main-2a0fac6d6
   syncPolicy:
     automated:
       prune: true
