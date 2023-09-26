4f20da9b065dc1913f7bf0e1e9c3a156fb9ee02e
Ashley Wang <ashley.wang@solo.io>
2023-09-26 16:46:13 -0400

    test ns

--- environments/gloo-core/core/clusterconfig/init.sh
+++ environments/gloo-core/core/clusterconfig/init.sh
@@ -5,4 +5,4 @@ echo "namespaces, configmaps, secrets"
 
 # argocd repo server check
 echo "checking that argocd-repo-server is ready before deploying wave"
-#$SCRIPT_DIR/tools/wait-for-rollout.sh deployment argocd-repo-server argocd 5
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment argocd-repo-server argocd 5
