8b40087a94707bf7adadd1fec2c94ce357f431e5
Ashley Wang <ashley.wang@solo.io>
2023-09-29 10:40:36 -0400

    Gloo Core cluster-1

--- environments/gloo-core/cluster1/README.md
+++ environments/gloo-core/cluster1/README.md
@@ -1,28 +1,28 @@
 # Environment Description
-The `gloo-core/core` environment deploys the core components of a single cluster Gloo Core demo, without any applications deployed. This is a great starting ground to get a barebones demo setup where you can bring your own application example into the mesh.
+The `gloo-platform/core/cluster1` environment deploys the `cluster1` worker for a multi-cluster Gloo Platform demo, which deploys Istio with ingress gateways and configures the Gloo Mesh Agent to communicate with the Gloo Mesh Control Plane served by the `gloo-platform/core/mgmt` environment
 
-![High Level Architecture](.images/core-arch-1a.png)
+![High Level Architecture](.images/gloo-platform-core-cluster1-arch-1a.png)
 
 ### Prerequisites
 - 1 Kubernetes Cluster
     - This demo has been tested on 1x `n2-standard-4` (gke), `m5.xlarge` (aws), or `Standard_DS3_v2` (azure) instance, and using K3d locally on M1 and Intel Macbook Pro
     - Kubernetes version 1.23-1.25
 
-## Environment descriptions
+## Environment description
 - base:
     - gloo mesh 2.5.0-gloo-core-alpha0
     - istio 1.18.3-solo (Helm)
     - revision: 1-18
-- lifecyclemanager:
+- ilcm:
     - gloo mesh 2.5.0-gloo-core-alpha0
-    - istio 1.18.3-solo (ILM)
+    - istio 1.18.3-solo (ILCM)
     - revision: 1-18
 
 ## Application description
 
 The RouteTables for applications exposed in this demo are defining non-wildcard hosts which follow the pattern `<app>.glootest.com`. You can map these hostnames to your gateway IP address in your DNS service of choice (i.e. Route53, Cloudflare), or you can follow the methods below to modify your `/etc/hosts` locally depending on your cluster LoadBalancer configuration.
 
-Applications Exposed using this demo:
+On the `mgmt` cluster gateway:
 - Homer Link Dashboard at `https://localhost` or `https://<LB Address>`
 - Grafana at `https://grafana.glootest.com`
 - ArgoCD at `https://argocd.glootest.com/argo`
---
+++ environments/gloo-core/cluster1/gloo-platform/init.sh
@@ -0,0 +1,188 @@
+#!/bin/bash
+
+echo "wave description:"
+echo "deploy and register gloo-mesh agent and addons"
+
+# check to see if gloo_mesh_version variable was passed through, if not prompt for it
+if [[ ${gloo_mesh_version} == "" ]]
+  then
+    # provide gloo_mesh_version variable
+    echo "Please provide the gloo_mesh_version to use (i.e. 2.5.0-gloo-core-alpha0):"
+    read gloo_mesh_version
+fi
+
+# discover gloo mesh endpoint with kubectl
+until [ "${SVC}" != "" ]; do
+  SVC=$(kubectl --context ${mgmt_context} -n gloo-mesh get svc gloo-mesh-mgmt-server -o jsonpath='{.status.loadBalancer.ingress[0].*}')
+  echo waiting for gloo mesh management server LoadBalancer IP to be detected
+  sleep 2
+done
+
+# discover gloo mesh metrics endpoint with kubectl
+until [ "${METRICS}" != "" ]; do
+  METRICS=$(kubectl --context ${mgmt_context} -n gloo-mesh get svc gloo-telemetry-gateway -o jsonpath='{.status.loadBalancer.ingress[0].*}')
+  echo waiting for gloo mesh metrics gateway LoadBalancer IP to be detected
+  sleep 2
+done
+
+kubectl apply --context ${mgmt_context} -f- <<EOF
+apiVersion: admin.gloo.solo.io/v2
+kind: KubernetesCluster
+metadata:
+  name: cluster1
+  namespace: gloo-mesh
+  labels:
+    roottrust: shared
+spec:
+  clusterDomain: cluster.local
+EOF
+
+# register clusters to gloo mesh with helm
+kubectl apply --context ${cluster_context} -f- <<EOF
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: gloo-platform-helm
+  namespace: argocd
+  finalizers:
+  - resources-finalizer.argocd.argoproj.io
+spec:
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: gloo-mesh
+  project: default
+  source:
+    chart: gloo-platform
+    helm:
+      skipCrds: true
+      values: |
+        common:
+            addonNamespace: "gloo-mesh-addons"
+            adminNamespace: "gloo-mesh"
+            cluster: cluster1
+        global: {}
+        glooAgent:
+            enabled: true
+            relay:
+                serverAddress: "${SVC}:9900"
+                clientTlsSecret:
+                    name: gloo-agent-tls-cert
+                    namespace: gloo-mesh
+
+    repoURL: https://storage.googleapis.com/gloo-platform-dev/platform-charts/helm-charts
+    targetRevision: 2.5.0-gloo-core-alpha0
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+  # ignore the self-signed certs that are being generated automatically    
+  ignoreDifferences:
+  - group: v1
+    kind: Secret
+EOF
+
+# deploy otel collector
+kubectl apply --context ${cluster_context} -f- <<EOF
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: gloo-platform-otel-collector
+  namespace: argocd
+  finalizers:
+  - resources-finalizer.argocd.argoproj.io
+spec:
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: gloo-mesh
+  project: default
+  source:
+    chart: gloo-platform
+    helm:
+      skipCrds: true
+      values: |
+        common:
+            addonNamespace: "gloo-mesh-addons"
+            adminNamespace: "gloo-mesh"
+            cluster: cluster1
+        telemetryCollector:
+            enabled: true
+            replicaCount: 1
+            mode: deployment
+            service: 
+              type: ClusterIP
+            podLabels:
+              istio.io/rev: 1-18
+            podAnnotations:
+              proxy.istio.io/config: '{ "holdApplicationUntilProxyStarts": true }'
+            image:
+              pullPolicy: IfNotPresent
+            config:
+                exporters:
+                    otlp:
+                        endpoint: "${METRICS}:4317"
+            extraVolumes:
+                - name: root-ca
+                  secret:
+                    defaultMode: 420
+                    secretName: gloo-telemetry-gateway-tls-secret
+                - configMap:
+                    items:
+                        - key: relay
+                          path: relay.yaml
+                    name: gloo-telemetry-collector-config
+                  name: telemetry-configmap
+                - hostPath:
+                    path: /var/run/cilium
+                    type: DirectoryOrCreate
+                  name: cilium-run
+        telemetryCollectorCustomization:
+            extraPipelines:
+                logs/gloo-mesh:
+                    receivers: [otlp/gloo-mesh]
+                    processors: [batch]
+                    exporters: 
+                        - logging/gloo-mesh
+                        # - <Add here your exporter>
+                traces/gloo-mesh: 
+                    receivers: [zipkin/gloo-mesh]
+                    processors: [batch]
+                    exporters:
+                        - otlp
+                        # - <Add here your exporter>
+                metrics/gloo-mesh:
+                    receivers: [prometheus]
+                    processors:
+                        - memory_limiter
+                        - filter/min
+                        - batch
+                        - attributes/drop_extra_istio_labels # Add this to drop labels not needed for Gloo Mesh UI.
+                        - attributes/drop_extra_otel_labels # Add this to drop some labels from the OTel collector itself.
+                        - gloo_metrics_processor
+                    exporters:
+                        - otlp
+            extraExporters:
+                logging/gloo-mesh:
+                    verbosity: normal
+                    sampling_initial: 5
+                    sampling_thereafter: 200
+            extraReceivers:
+                zipkin/gloo-mesh:
+                    endpoint: 0.0.0.0:9411
+                otlp/gloo-mesh:
+                  protocols:
+                    grpc:
+                      endpoint: 0.0.0.0:4317
+                    http:
+                      endpoint: 0.0.0.0:4318
+                  
+    repoURL: https://storage.googleapis.com/gloo-platform-dev/platform-charts/helm-charts
+    targetRevision: 2.5.0-gloo-core-alpha0
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+  # ignore the self-signed certs that are being generated automatically    
+  ignoreDifferences:
+  - group: v1
+    kind: Secret
+EOF
