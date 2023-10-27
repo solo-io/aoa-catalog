383e1ac62e5a1e96c02c8041c6c77db7573a8dd2
Alex <alexbrucely@gmail.com>
2023-10-27 14:18:16 -0700

    Update test.sh

--- environments/gloo-platform/glooplatform-central/tiered-app/test.sh
+++ environments/gloo-platform/glooplatform-central/tiered-app/test.sh
@@ -2,4 +2,4 @@
 
 # wait for completion of app install
 #$SCRIPT_DIR/tools/wait-for-rollout.sh deployment httpbin httpbin 10 ${cluster_context}
-$SCRIPT_DIR/tools/wait-for-rollout.sh deployment throbbing-field-v1 ns-1 10 ${cluster_context}
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment restless-sky-v1 ns-1 10 ${cluster_context}
