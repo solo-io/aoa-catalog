b171394499382ed14aafd532f4bbeebb1cbe1224
Alex <alexbrucely@gmail.com>
2023-11-07 13:01:00 -0800

    update

--- environments/gloo-platform/glooplatform-east/tiered-app/test.sh
+++ environments/gloo-platform/glooplatform-east/tiered-app/test.sh
@@ -2,4 +2,4 @@
 
 # wait for completion of app install
 #$SCRIPT_DIR/tools/wait-for-rollout.sh deployment httpbin httpbin 10 ${cluster_context}
-$SCRIPT_DIR/tools/wait-for-rollout.sh deployment friendly-guy-v1 ns-1 10 ${cluster_context}
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment strangler-pattern-v1 ns-1 10 ${cluster_context}
