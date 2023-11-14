dcf352e887e41b3bf3b1537f1e2e50faf5efd7a0
Alex <alexbrucely@gmail.com>
2023-11-14 15:51:40 -0800

    Revert "update host"
    This reverts commit c86d07fd28f5c3c099eea8f5a3d5a6163709bd32.

--- environments/gloo-edge/edge.glooplatform.com/solo-dev-portal-config/test.sh
+++ environments/gloo-edge/edge.glooplatform.com/solo-dev-portal-config/test.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+echo 
+echo "installation complete:"
+echo
+echo "To access applications, follow the methods below:"
+echo 
+echo "Method 1 - LoadBalancer External-IP: modify /etc/hosts on your local machine (this will require sudo privileges)"
+echo "cat <<EOF | sudo tee -a /etc/hosts"
+echo "$(kubectl -n gloo-system get service gateway-proxy -o jsonpath='{.status.loadBalancer.ingress[0].ip}') argocd.glootest.com api.glootest.com apidev.glootest.com portal.glootest.com"
+echo "EOF"
+echo
+echo "Method 2 - K3d LB Integration: modify /etc/hosts on your local machine (this will require sudo privileges)"
+echo "cat <<EOF | sudo tee -a /etc/hosts"
+echo "127.0.0.1 argocd.glootest.com api.glootest.com apidev.glootest.com portal.glootest.com"
+echo "EOF"
+echo
+echo "access argocd at https://argocd.glootest.com/argo"
+echo "access the solo dev portal application at: https://portal.glootest.com"
+echo
+echo "Method 3: use port-forwarding"
+echo "alternatively, access argocd using port-forward command:" 
+echo "kubectl port-forward svc/argocd-server -n argocd 9999:443"
+echo
+echo "argocd credentials:"
+echo "user: admin"
+echo "password: solo.io"
+echo
+echo "access argocd at https://localhost:9999/argo"
+echo 
