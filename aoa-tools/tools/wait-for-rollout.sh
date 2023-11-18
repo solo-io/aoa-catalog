c5458c75f9db1f1a4ffa624d4c5e08b6355c5c72
Alex <alexbrucely@gmail.com>
2023-11-17 18:22:26 -0800

    using refactored aoa-tools

--- aoa-tools/tools/wait-for-rollout.sh
+++ aoa-tools/tools/wait-for-rollout.sh
@@ -2,29 +2,28 @@
 
 seconds=0
 OUTPUT=0
-# example rollout would be deployments/<deployment_name>
+
+# Example rollout would be deployments/<deployment_name>
 rollout_type=$1
 rollout_name=$2
 namespace=$3
 period=$4
 context=$5
 
-### If the context is not specified, simply use the default context.
-if [[ ${context} == "" ]]
-then
-  context=`kubectl config current-context`
-  if [[ ${context} == "" ]]
-  then
-    echo "You do not have a curent kubernetes cluster.  Please create one."
+# If the context is not specified, use the default context
+if [[ ${context} == "" ]]; then
+  context=$(kubectl config current-context)
+  if [[ ${context} == "" ]]; then
+    echo "You do not have a current Kubernetes cluster. Please create one."
     exit 1
   fi
   echo "No context specified. Using current context of ${context}"
 fi
 
 while [ "$OUTPUT" -ne 1 ]; do
-  OUTPUT=`kubectl get ${rollout_type}/${rollout_name} -n ${namespace} --context ${context} 2>/dev/null | grep -c ${rollout_name}`;
-  seconds=$((seconds+${period}))
-  printf "Waiting %s seconds for ${rollout_type} ${rollout_name} to come up.\n" "${seconds}"
+  OUTPUT=$(kubectl get ${rollout_type}/${rollout_name} -n ${namespace} --context ${context} 2>/dev/null | grep -c ${rollout_name})
+  seconds=$((seconds + ${period}))
+  printf "Waiting %s seconds for %s %s to come up.\n" "${seconds}" "${rollout_type}" "${rollout_name}"
   sleep ${period}
   kubectl rollout status ${rollout_type}/${rollout_name} -n ${namespace} --context ${context}
 done
