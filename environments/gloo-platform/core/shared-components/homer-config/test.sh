0744ccd498e5068b6100841c8e7206eb5053a8f8
Alex <alexbrucely@gmail.com>
2023-11-20 17:25:16 -0800

    Revert "start simple"
    This reverts commit c8cbd53ba3a857c21fb0ba2c4ff5ac0138b08047.

--- environments/gloo-platform/core/shared-components/homer-config/test.sh
+++ environments/gloo-platform/core/shared-components/homer-config/test.sh
@@ -9,7 +9,7 @@ echo "if running locally with K3d:"
 echo "access the dashboard at https://localhost/solo"
 echo
 echo "If using LoadBalancer External-IP:"
-echo "access the dashboard at https://$(kubectl -n istio-gateways get service istio-ingressgateway-${ISTIO_REVISION} -o jsonpath='{.status.loadBalancer.ingress[0].*}')/solo"
+echo "access the dashboard at https://$(kubectl -n istio-gateways get service istio-ingressgateway-${ISTIO_REVISION} -o jsonpath='{.status.loadBalancer.ingress[0].ip}')/solo"
 echo
 echo "Additional details on hostname entries for this demo environment will be provided in the Welcome section, but is also described in the README section below:"
 echo "https://github.com/solo-io/aoa-catalog/tree/main/environments"
