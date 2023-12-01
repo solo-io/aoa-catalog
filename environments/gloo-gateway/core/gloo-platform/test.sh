b31c03346d2f5b2ee7b809f3ffe8c851ec4e36c0
Alex <alexbrucely@gmail.com>
2023-12-01 09:41:35 -0800

    update the wait command for Gloo Platform CRDs in `core/gloo-platform/test.sh` to run silently

--- environments/gloo-gateway/core/gloo-platform/test.sh
+++ environments/gloo-gateway/core/gloo-platform/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until kubectl --context ${cluster_context} -n gloo-mesh wait --for condition=established crd/kubernetesclusters.admin.gloo.solo.io ; do sleep 10; done
+until kubectl --context ${cluster_context} -n gloo-mesh wait --for condition=established crd/kubernetesclusters.admin.gloo.solo.io > /dev/null 2>&1; do sleep 10; done
 
 kubectl apply --context ${cluster_context} -f- <<EOF
 apiVersion: admin.gloo.solo.io/v2
