56ee1abf7796477e697facb174eda6045c5550e7
Alex <alexbrucely@gmail.com>
2023-11-30 18:13:15 -0800

    update

--- environments/istio/ambient/loadgenerators/test.sh
+++ environments/istio/ambient/loadgenerators/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # wait for completion of httpbin install
-$SCRIPT_DIR/tools/wait-for-rollout.sh deployment plow-ns-1 istio-system 10 ${cluster_context}
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment bombardier-ns-1 ns-1 10 ${cluster_context}
 
