ac4d7bb4b9b786f3f1d7161b829340eb1488a030
Alex <alexbrucely@gmail.com>
2023-12-22 11:48:52 -0800

    test

--- environments/gloo-edge/ggv2/gloo-gateway-v2-app/test.sh
+++ environments/gloo-edge/ggv2/gloo-gateway-v2-app/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # wait for gloo edge deployment
-#$SCRIPT_DIR/tools/wait-for-rollout.sh deployment gloo gloo-system 10
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment glood-gloo-gateway-v2 gloo-system 10
