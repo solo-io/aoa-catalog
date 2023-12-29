0e735e4d815baa6c90477d4b91c557a27bbecdc6
Alex <alexbrucely@gmail.com>
2023-12-29 11:33:00 -0800

    fix selector

--- environments/gloo-platform/core/shared-components/homer-config/test.sh
+++ environments/gloo-platform/core/shared-components/homer-config/test.sh
@@ -7,7 +7,7 @@ echo "if running locally with K3d:"
 echo "access the dashboard at https://localhost/solo"
 echo
 echo "If using LoadBalancer External-IP:"
-echo "access the dashboard at https://$(kubectl --context ${mgmt_context} get svc -n istio-gateways --selector=istio=ingressgateway -o jsonpath='{.items[*].status.loadBalancer.ingress[0].*}')/solo"
+echo "access the dashboard at https://$(kubectl --context ${mgmt_context} get svc -n istio-gateways --selector=istio=mgmt-ingressgateway -o jsonpath='{.items[*].status.loadBalancer.ingress[0].*}')/solo"
 echo
 echo "Additional details on hostname entries for this demo environment will be provided in the Welcome section, but is also described in the README section below:"
 echo "https://github.com/solo-io/aoa-catalog/tree/main/environments"
