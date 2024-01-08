c3ec2f7d07ca7d0b8864a77c45415942162524c8
Adam Sayah <adam.sayah@solo.io>
2024-01-08 15:27:57 -0700

    adding token to gitlab

--- environments/gloo-platform/mgmt/mesh-factory-config/test.sh
+++ environments/gloo-platform/mgmt/mesh-factory-config/test.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+kubectl --context $cluster_context -n gitlab exec -it deploy/gitlab -- gitlab-rails runner  "token = User.find_by_username('root').personal_access_tokens.create(scopes: ['api', 'read_api', 'read_user', 'create_runner', 'read_repository', 'write_repository', 'sudo', 'admin_mode' ], name: 'Automation token 2', expires_at: 365.days.from_now); token.set_token('solo-token-123'); token.save!"
