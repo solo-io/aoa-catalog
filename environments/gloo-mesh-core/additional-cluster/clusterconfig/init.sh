e68d80508d0b48d5272d06e0da20add9af6392c7
Alex <alexbrucely@gmail.com>
2024-02-05 16:20:11 -0800

    dont need license

--- environments/gloo-mesh-core/additional-cluster/clusterconfig/init.sh
+++ environments/gloo-mesh-core/additional-cluster/clusterconfig/init.sh
@@ -6,6 +6,3 @@ echo "namespaces, configmaps, secrets"
 # argocd repo server check
 echo "checking that argocd-repo-server is ready before deploying wave"
 $SCRIPT_DIR/tools/wait-for-rollout.sh deployment argocd-repo-server argocd 5
-
-# create license
-$SCRIPT_DIR/tools/create-license.sh "${license_key}" "${cluster_context}"
