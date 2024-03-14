a7ffa963bc3e541cfffdf9e8d883ce516549a6cb
Alex <alexbrucely@gmail.com>
2024-03-13 19:01:58 -0700

    test ambient app demo update

--- environments/istio/ambient-demo/gke/helm-deploy-1.20/apps/test.sh
+++ environments/istio/ambient-demo/gke/helm-deploy-1.20/apps/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # wait for completion of app install
-#$SCRIPT_DIR/tools/wait-for-rollout.sh deployment httpbin httpbin 10 ${cluster_context}
-#$SCRIPT_DIR/tools/wait-for-rollout.sh deployment throbbing-field-v1 ns-1 10 ${cluster_context}
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment tier-1-app-a ns-5 10 ${cluster_context}
