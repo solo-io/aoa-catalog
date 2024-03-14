ed869d4e80f602537116b8a435bd8e84e016f29a
Alex <alexbrucely@gmail.com>
2024-03-14 15:34:13 -0700

    test

--- environments/istio/ambient-demo/gke/operator-deploy-1.19/apps/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# wait for completion of app install
-#$SCRIPT_DIR/tools/wait-for-rollout.sh deployment httpbin httpbin 10 ${cluster_context}
-#$SCRIPT_DIR/tools/wait-for-rollout.sh deployment throbbing-field-v1 ns-1 10 ${cluster_context}
