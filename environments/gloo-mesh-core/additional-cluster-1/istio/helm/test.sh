235e100045bbd5747fa0d8478af27229402bd777
Alex <alexbrucely@gmail.com>
2024-04-04 08:28:00 -0700

    remove pointers to OCP as these are unused/untested

--- environments/gloo-mesh-core/additional-cluster-1/istio/helm/test.sh
+++ environments/gloo-mesh-core/additional-cluster-1/istio/helm/test.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-if [ "${environment_overlay}" == "ocp" ] ; then 
-     $SCRIPT_DIR/tools/wait-for-rollout.sh deployment istio-ingressgateway-1-20 istio-gateways 10 ${cluster_context}
-     #./tools/wait-for-rollout.sh deployment istio-eastwestgateway-1-20 istio-eastwest 10 ${cluster_context}
-  else
-     $SCRIPT_DIR/tools/wait-for-rollout.sh deployment istio-ingressgateway-1-20 istio-gateways 10 ${cluster_context}
-     #./tools/wait-for-rollout.sh deployment istio-eastwestgateway-1-20 istio-eastwest 10 ${cluster_context}
-  fi
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment istio-ingressgateway-1-20 istio-gateways 10 ${cluster_context}
