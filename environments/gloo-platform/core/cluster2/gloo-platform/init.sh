e3b13aa9106338e889f07a2e11cb23c0dc79d88b
Ashley Wang <ashwang168@gmail.com>
2024-05-10 11:09:46 -0400

    update

--- environments/gloo-platform/core/cluster2/gloo-platform/init.sh
+++ environments/gloo-platform/core/cluster2/gloo-platform/init.sh
@@ -74,7 +74,7 @@ spec:
                   name: null
                   namespace: null
                   
-    repoURL: https://storage.googleapis.com/gloo-platform/helm-charts
+    repoURL: https://storage.googleapis.com/gloo-platform-dev/platform-charts/helm-charts
     targetRevision: 2.5.0
   syncPolicy:
     automated:
@@ -144,7 +144,7 @@ spec:
                       type: DirectoryOrCreate
                   name: cilium-run
                   
-    repoURL: https://storage.googleapis.com/gloo-platform/helm-charts
+    repoURL: https://storage.googleapis.com/gloo-platform-dev/platform-charts/helm-charts
     targetRevision: 2.5.0
   syncPolicy:
     automated:
