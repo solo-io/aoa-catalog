de9cec0831d59a8edc2819887d63b65abcd6a5c0
Alex <alexbrucely@gmail.com>
2024-05-30 09:26:00 -0700

    Update test.sh

--- environments/gloo-mesh-core/singlecluster/homer-app/glooplatform.com/test.sh
+++ environments/gloo-mesh-core/singlecluster/homer-app/glooplatform.com/test.sh
@@ -8,5 +8,5 @@ echo "A homepage has been exposed to simplify navigation"
 echo "access the dashboard at https://demo.admin.gmc.glooplatform.com/solo"
 echo
 echo "If using LoadBalancer External-IP:"
-echo "access the dashboard at https://$(kubectl -n istio-gateways get service istio-ingressgateway-${ISTIO_REVISION} -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
+echo "access the dashboard at https://$(kubectl -n istio-gateways get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
 echo
