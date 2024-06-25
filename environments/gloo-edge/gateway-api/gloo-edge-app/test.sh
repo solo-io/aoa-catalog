96b67c7695a9134e3c4987ceeb102ef12e5746f3
Alex <alexbrucely@gmail.com>
2024-06-24 17:08:19 -0700

    Update test.sh

--- environments/gloo-edge/gateway-api/gloo-edge-app/test.sh
+++ environments/gloo-edge/gateway-api/gloo-edge-app/test.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+# wait for gloo control plane deployment
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment gloo gloo-system 10
+
+# wait for gloo proxy deployment
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment gloo-proxy-https gloo-system 10
