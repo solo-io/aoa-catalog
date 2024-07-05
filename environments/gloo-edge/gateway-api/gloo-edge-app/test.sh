aced51c1feb0b8dbb23cbf8a54a3f81a47fd981a
Alex <alexbrucely@gmail.com>
2024-07-05 12:10:33 -0700

    update

--- environments/gloo-edge/gateway-api/gloo-edge-app/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# wait for gloo control plane deployment
-$SCRIPT_DIR/tools/wait-for-rollout.sh deployment gloo gloo-system 10
-
-# wait for gloo proxy deployment
-$SCRIPT_DIR/tools/wait-for-rollout.sh deployment gloo-proxy-https gloo-system 10
--- environments/gloo-edge/gateway-api/homer-app/test.sh
+++ environments/gloo-edge/gateway-api/homer-app/test.sh
@@ -8,7 +8,7 @@ echo
 echo "The applications featured in this demo are hosted under the *.edge.glooplatform.com domain"
 echo
 echo "When using K3d locally with the -i flag, add the entries below to your /etc/hosts file:"
-echo "127.0.0.1 argocd.edge.glooplatform.com homer.edge.glooplatform.com httpbin.edge.glooplatform.com"
+echo "127.0.0.1 argocd.edge.glooplatform.com homer.edge.glooplatform.com httpbin.edge.glooplatform.com bookinfo.edge.glooplatform.com general-chatbot.edge.glooplatform.com language-chatbot.edge.glooplatform.com"
 echo 
 echo "Otherwise, map the hostnames above to your Load Balancer IP address using your DNS solution of choice (i.e. etc/hosts, CloudFlare, Route53, etc.)"
 echo
