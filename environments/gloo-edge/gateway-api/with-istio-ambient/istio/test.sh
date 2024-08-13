b34fb71ec20e2bbc967131480391711db83da7d0
Alex <alexbrucely@gmail.com>
2024-08-13 08:45:11 -0700

    test

---
+++ environments/gloo-edge/gateway-api/with-istio-ambient/istio/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+$SCRIPT_DIR/tools/wait-for-rollout.sh ds istio-cni-node kube-system 10 ${cluster_context}
+$SCRIPT_DIR/tools/wait-for-rollout.sh ds ztunnel istio-system 10 ${cluster_context}
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment istio-ingressgateway istio-system 10 ${cluster_context}
