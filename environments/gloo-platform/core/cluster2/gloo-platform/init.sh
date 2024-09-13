fddf8f94f87184a9faf72886dbadac7a3a4927ed
Ashley Wang <ashwang168@gmail.com>
2024-09-13 11:03:38 -0400

    2.6.3

--- environments/gloo-platform/core/cluster2/gloo-platform/init.sh
+++ environments/gloo-platform/core/cluster2/gloo-platform/init.sh
@@ -7,7 +7,7 @@ echo "deploy and register gloo-mesh agent and addons"
 if [[ ${gloo_mesh_version} == "" ]]
   then
     # provide gloo_mesh_version variable
-    echo "Please provide the gloo_mesh_version to use (i.e. 2.7.0-beta0-2024-09-05-ashleywang1-default-release-namespace-for-namespaced-rbac-c54c67f35d):"
+    echo "Please provide the gloo_mesh_version to use (i.e. 2.6.3-2024-09-07-v2.6.x-685df442d7):"
     read gloo_mesh_version
 fi
 
@@ -75,7 +75,7 @@ spec:
                   namespace: null
                   
     repoURL: https://storage.googleapis.com/gloo-platform-dev/platform-charts/helm-charts
-    targetRevision: 2.7.0-beta0-2024-09-05-ashleywang1-default-release-namespace-for-namespaced-rbac-c54c67f35d
+    targetRevision: 2.6.3-2024-09-07-v2.6.x-685df442d7
   syncPolicy:
     automated:
       prune: true
@@ -123,7 +123,7 @@ spec:
             image:
               pullPolicy: IfNotPresent
               repository: gcr.io/gloo-mesh/gloo-otel-collector
-              tag: 2.7.0-beta0-2024-09-05-ashleywang1-default-release-namespace-for-namespaced-rbac-c54c67f35d
+              tag: 2.6.3-2024-09-07-v2.6.x-685df442d7
             config:
                 exporters:
                     otlp:
@@ -145,7 +145,7 @@ spec:
                   name: cilium-run
                   
     repoURL: https://storage.googleapis.com/gloo-platform-dev/platform-charts/helm-charts
-    targetRevision: 2.7.0-beta0-2024-09-05-ashleywang1-default-release-namespace-for-namespaced-rbac-c54c67f35d
+    targetRevision: 2.6.3-2024-09-07-v2.6.x-685df442d7
   syncPolicy:
     automated:
       prune: true
--- environments/gloo-platform/multicluster-bookinfo-httpbin/mgmt/README.md
+++ environments/gloo-platform/multicluster-bookinfo-httpbin/mgmt/README.md
@@ -16,11 +16,11 @@ When applied with `cluster1` and `cluster2` environments, here is a high level d
 
 ## Environment description
 - base:
-    - gloo mesh 2.7.0-beta0-2024-09-05-ashleywang1-default-release-namespace-for-namespaced-rbac-c54c67f35d
+    - gloo mesh 2.6.3-2024-09-07-v2.6.x-685df442d7
     - istio 1.22.3-patch1-solo (Helm)
     - revision: 1-22
 - ilcm:
-    - gloo mesh 2.7.0-beta0-2024-09-05-ashleywang1-default-release-namespace-for-namespaced-rbac-c54c67f35d
+    - gloo mesh 2.6.3-2024-09-07-v2.6.x-685df442d7
     - istio 1.22.3-patch1-solo (ILCM)
     - revision: 1-22
 
