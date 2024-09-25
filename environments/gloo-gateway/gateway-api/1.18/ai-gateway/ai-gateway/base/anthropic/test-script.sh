5656c78c90ad7d4af9bbd8444898389488913c85
Alex <alexbrucely@gmail.com>
2024-09-25 11:51:22 -0700

    update

---
+++ environments/gloo-gateway/gateway-api/1.18/ai-gateway/ai-gateway/base/anthropic/test-script.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+export INGRESS_GW_ADDRESS=$(kubectl get svc -n gloo-system gloo-proxy-ai-gateway -o jsonpath="{.status.loadBalancer.ingress[0]['hostname','ip']}")
+echo $INGRESS_GW_ADDRESS
+
+curl -v "$INGRESS_GW_ADDRESS:8080/anthropic" -H content-type:application/json  -d '{
+  "model": "gpt-3.5-turbo",
+  "messages": [
+    {
+      "role": "system",
+      "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
+    },
+    {
+      "role": "user",
+      "content": "Compose a poem that explains the concept of recursion in programming."
+    }
+  ]
+}' | jq
--- environments/gloo-gateway/gateway-api/1.18/ai-gateway/ai-gateway/init.sh
+++ environments/gloo-gateway/gateway-api/1.18/ai-gateway/ai-gateway/init.sh
@@ -15,6 +15,18 @@ if [[ ${openai_api_key} == "" ]]
     read openai_api_key
 fi
 
+# check to see if llm key variables were passed through, if not prompt for keys
+if [[ ${anthropic_api_key} == "" ]]
+  then
+    # provide license key
+    echo "Key not found in env.var file, please provide your OpenAI API key:"
+    read anthropic_api_key
+fi
+
 kubectl create secret generic openai-secret -n gloo-system \
 --from-literal="Authorization=Bearer $openai_api_key" \
+--dry-run=client -oyaml | kubectl apply -f -
+
+kubectl create secret generic anthropic-secret -n gloo-system \
+--from-literal="Authorization=Bearer $anthropic_api_key" \
 --dry-run=client -oyaml | kubectl apply -f -
