1369836bf4c2f3427b2c147281186c98cfe084e6
Alex <alexbrucely@gmail.com>
2024-10-11 15:59:58 -0700

    Update test.sh

--- environments/gloo-gateway/gateway-api/live/chatbot/test.sh
+++ environments/gloo-gateway/gateway-api/live/chatbot/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # wait for gloo chatbot deployment
-$SCRIPT_DIR/tools/wait-for-rollout.sh deployment general-chat-deployment ai-chatbot 10
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment betterchatgpt-deployment ai-chatbot 10
