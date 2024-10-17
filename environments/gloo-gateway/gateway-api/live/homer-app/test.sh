782e708fed4256cefa1c652583943b6236ff85de
Alex <alexbrucely@gmail.com>
2024-10-17 10:40:27 -0700

    Update test.sh

--- environments/gloo-gateway/gateway-api/live/homer-app/test.sh
+++ environments/gloo-gateway/gateway-api/live/homer-app/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$SCRIPT_DIR/tools/wait-for-rollout.sh deployment homer-portal homer-portal 10 ${cluster_context}
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment homer-portal-solo homer-portal 10 ${cluster_context}
 
 echo 
 echo "Installation complete:"
