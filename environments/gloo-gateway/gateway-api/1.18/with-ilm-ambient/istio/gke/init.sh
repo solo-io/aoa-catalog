1f05a8788e3a40102c8b33e71d85a984eb0c4e19
Alex <alexbrucely@gmail.com>
2024-12-24 15:18:30 -0800

    update

--- environments/gloo-gateway/gateway-api/1.18/with-ilm-ambient/istio/gke/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "wave description:"
-echo "istiod and ingress gateway deployments"
-
-# install Kubernetes Gateway CRDs
-echo "installing Kubernetes Gateway CRDs"
-kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
-  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v0.6.1" | kubectl apply -f -; }
--- environments/gloo-gateway/gateway-api/1.18/with-ilm-ambient/istio/init.sh
+++ environments/gloo-gateway/gateway-api/1.18/with-ilm-ambient/istio/init.sh
@@ -2,8 +2,3 @@
 
 echo "wave description:"
 echo "istiod and ingress gateway deployments"
-
-# install Kubernetes Gateway CRDs
-echo "installing Kubernetes Gateway CRDs"
-kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
-  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v0.6.1" | kubectl apply -f -; }
