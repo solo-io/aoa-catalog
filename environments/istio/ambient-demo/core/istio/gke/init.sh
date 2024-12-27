deab6e0f80ae8cd45c5136e8af9142d65ae1e654
Alex <alexbrucely@gmail.com>
2024-12-27 13:16:59 -0800

    update

---
+++ environments/istio/ambient-demo/core/istio/gke/init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "wave description:"
+echo "istiod and ingress gateway deployments"
+
+# install Kubernetes Gateway CRDs
+echo "installing Kubernetes Gateway CRDs"
+kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
+  { kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.0/standard-install.yaml; }
+
+# apply resourcequota for istio-system
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: ResourceQuota
+metadata:
+  name: gcp-critical-pods
+  namespace: istio-system
+spec:
+  hard:
+    pods: 1000
+  scopeSelector:
+    matchExpressions:
+    - operator: In
+      scopeName: PriorityClass
+      values:
+      - system-node-critical
+EOF
