8d3a16c1ff303379186d1a427e83a4e2654556cc
Alex <alexbrucely@gmail.com>
2024-12-27 11:52:07 -0800

    istio updates

--- environments/gloo-gateway/gateway-api/1.17/with-istio-ambient/istio/init.sh
+++ environments/gloo-gateway/gateway-api/1.17/with-istio-ambient/istio/init.sh
@@ -6,4 +6,4 @@ echo "istiod and ingress gateway deployments"
 # install Kubernetes Gateway CRDs
 echo "installing Kubernetes Gateway CRDs"
 kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
-  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v0.6.1" | kubectl apply -f -; }
+  { kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.0/standard-install.yaml; }
