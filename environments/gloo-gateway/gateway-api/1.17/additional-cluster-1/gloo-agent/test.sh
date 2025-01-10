3390d589f38a0a94aa7e734e64879300f8b818d6
Ashley Wang <ashwang168@gmail.com>
2025-01-10 11:39:56 -0500

    update

--- environments/gloo-gateway/gateway-api/1.17/additional-cluster-1/gloo-agent/test.sh
+++ environments/gloo-gateway/gateway-api/1.17/additional-cluster-1/gloo-agent/test.sh
@@ -4,7 +4,7 @@
 if [[ ${gloo_mesh_version} == "" ]]
   then
     # provide gloo_mesh_version variable
-    echo "Please provide the gloo_mesh_version to use (i.e. 2.7.0-beta1-2025-01-07-jmcguire-check-crds-installed-before-starting-controllers-930c24925b):"
+    echo "Please provide the gloo_mesh_version to use (i.e. 2.7.0-beta1-2025-01-08-jhawley-apiserver-no-cross-cluster-57598e7e27):"
     read gloo_mesh_version
 fi
 
@@ -105,7 +105,7 @@ spec:
             enabled: true
 
     repoURL: https://storage.googleapis.com/gloo-platform-dev/platform-charts/helm-charts
-    targetRevision: 2.7.0-beta1-2025-01-07-jmcguire-check-crds-installed-before-starting-controllers-930c24925b
+    targetRevision: 2.7.0-beta1-2025-01-08-jhawley-apiserver-no-cross-cluster-57598e7e27
   syncPolicy:
     automated:
       prune: true
@@ -163,7 +163,7 @@ spec:
             skipVerify: true
 
     repoURL: https://storage.googleapis.com/gloo-platform-dev/platform-charts/helm-charts
-    targetRevision: 2.7.0-beta1-2025-01-07-jmcguire-check-crds-installed-before-starting-controllers-930c24925b
+    targetRevision: 2.7.0-beta1-2025-01-08-jhawley-apiserver-no-cross-cluster-57598e7e27
   syncPolicy:
     automated:
       prune: true
