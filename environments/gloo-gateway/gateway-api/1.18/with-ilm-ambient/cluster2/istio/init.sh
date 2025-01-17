e9473e97534807775cf009a2736fcb0788739c6b
Alex <alexbrucely@gmail.com>
2025-01-16 16:54:11 -0800

    cluster2

--- environments/gloo-gateway/gateway-api/1.18/with-ilm-ambient/cluster2/istio/init.sh
+++ environments/gloo-gateway/gateway-api/1.18/with-ilm-ambient/cluster2/istio/init.sh
@@ -2,3 +2,6 @@
 
 echo "wave description:"
 echo "istiod and ingress gateway deployments"
+
+kubectl create ns istio-system --context ${cluster_context}
+kubectl apply -f $SCRIPT_DIR/shared-root-trust-secret.yaml --context ${cluster_context}
