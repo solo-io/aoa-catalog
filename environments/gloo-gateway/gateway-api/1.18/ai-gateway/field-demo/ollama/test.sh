fe40ec0486fbfc91f255d68a3bf2530a3927fb55
Alex <alexbrucely@gmail.com>
2025-01-27 16:51:10 -0800

    Update test.sh

--- environments/gloo-gateway/gateway-api/1.18/ai-gateway/field-demo/ollama/test.sh
+++ environments/gloo-gateway/gateway-api/1.18/ai-gateway/field-demo/ollama/test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # wait for ollama qwen deployment
-$SCRIPT_DIR/tools/wait-for-rollout.sh deployment ollama-qwen ollama 10
+#$SCRIPT_DIR/tools/wait-for-rollout.sh deployment ollama-qwen ollama 10
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment ollama-deepseek ollama 10
 
 echo "navigate to the following directory to run through AI gateway demos:"
 echo "cd environments/gloo-gateway/gateway-api/1.18/ai-gateway/field-demo/demos"
