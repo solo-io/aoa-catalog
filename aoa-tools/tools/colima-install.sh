3eb165d2602b55900924a6938b4eb8a6aa7f3b04
Alex <alexbrucely@gmail.com>
2025-01-27 22:48:38 -0800

    Update colima-install.sh

--- aoa-tools/tools/colima-install.sh
+++ aoa-tools/tools/colima-install.sh
@@ -16,10 +16,6 @@ colima start --cpu 6 --memory 16 \
   --network-address \
   --k3s-arg "--disable=traefik"
 
-# Setup colima for ssh capability
-printf "=== Installing socat\n"
-colima ssh -- sudo apt install -y socat
-
 # Add node labels
 kubectl label nodes colima topology.kubernetes.io/region=us-west
 kubectl label nodes colima topology.kubernetes.io/zone=us-west-1
