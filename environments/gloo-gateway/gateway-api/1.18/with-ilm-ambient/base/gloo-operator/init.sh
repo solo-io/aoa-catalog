da83a682aff8600d73229af82ca6b3c9e076aa74
Alex <alexbrucely@gmail.com>
2025-01-28 12:10:43 -0800

    update gwapi crds to v1.2.0

--- environments/gloo-gateway/gateway-api/1.18/with-ilm-ambient/base/gloo-operator/init.sh
+++ environments/gloo-gateway/gateway-api/1.18/with-ilm-ambient/base/gloo-operator/init.sh
@@ -4,3 +4,7 @@ echo "wave description:"
 echo "deploying gloo-operator"
 
 kubectl create namespace gloo-system --context ${cluster_context}
+
+# Install Gateway API CRDs
+echo "installing Gateway API CRDs"
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.0/standard-install.yaml
