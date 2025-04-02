02d82505e80139659cdc04be143d1c5f1ef988ac
Alex <alexbrucely@gmail.com>
2025-04-02 15:20:25 -0700

    Update init.sh

--- environments/gloo-gateway/gateway-api/live/kagent/init.sh
+++ environments/gloo-gateway/gateway-api/live/kagent/init.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
 echo "wave description:"
-echo "deploy ai-gateway"
-
-echo "this demo requires the following API keys configured in the vars.env, if not provided the script will prompt for them:"
-echo "openai_api_key"
-
-# check to see if llm key variables were passed through, if not prompt for keys
-if [[ ${openai_api_key} == "" ]]
-  then
-    # provide license key
-    echo "Key not found in env.var file, please provide your OpenAI API key:"
-    read openai_api_key
-fi
-
-kubectl create secret generic kagent-openai -n kagent \
-  --from-literal="OPENAI_API_KEY=$openai_api_key" \
-  --dry-run=client -oyaml | kubectl apply -f -
+echo "deploy kagent"
 
 helm upgrade -i kagent oci://ghcr.io/kagent-dev/kagent/helm/kagent \
     --namespace kagent \
     --create-namespace \
     --version 0.1.11 \
-    --set openai.secretName="kagent-openai"
+    --set openai.apiKey=$OPENAI_API_KEY
