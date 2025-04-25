8979396768a10a6cd1370b5947c82f6270b361f6
Alex <alexbrucely@gmail.com>
2025-04-24 17:29:34 -0700

    update kagent to 0.2.0 and add to homer

--- environments/gloo-gateway/gateway-api/live/kagent/init.sh
+++ environments/gloo-gateway/gateway-api/live/kagent/init.sh
@@ -3,8 +3,14 @@
 echo "wave description:"
 echo "deploy kagent"
 
+helm upgrade -i kagent-crds oci://ghcr.io/kagent-dev/kagent/helm/kagent-crds \
+    --namespace kagent \
+    --create-namespace \
+    --version 0.2.0
+
 helm upgrade -i kagent oci://ghcr.io/kagent-dev/kagent/helm/kagent \
     --namespace kagent \
     --create-namespace \
-    --version 0.1.11 \
+    --skip-crds \
+    --version 0.2.0 \
     --set openai.apiKey=$openai_api_key
