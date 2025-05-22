47a33582cdb444a172418bb00b2c6edf399afbd5
Alex <alexbrucely@gmail.com>
2025-05-22 15:26:07 -0700

    update kagent
    - version from 0.2.0 to 0.3.9
    - new value for key

--- environments/gloo-gateway/gateway-api/live/kagent/init.sh
+++ environments/gloo-gateway/gateway-api/live/kagent/init.sh
@@ -6,11 +6,11 @@ echo "deploy kagent"
 helm upgrade -i kagent-crds oci://ghcr.io/kagent-dev/kagent/helm/kagent-crds \
     --namespace kagent \
     --create-namespace \
-    --version 0.2.0
+    --version 0.3.9
 
 helm upgrade -i kagent oci://ghcr.io/kagent-dev/kagent/helm/kagent \
     --namespace kagent \
     --create-namespace \
     --skip-crds \
-    --version 0.2.0 \
-    --set openai.apiKey=$openai_api_key
+    --version 0.3.9 \
+    --set providers.openAI.apiKey=$openai_api_key
